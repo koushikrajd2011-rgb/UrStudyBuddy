d53f1a02c85bff9e93cb2286042e6f6a71262da3
ImNotPro <koushikraj.d2011@gmail.com>
2026-09-08 16:43:04 +0800

    updates env file

--- api/login.js
+++ api/login.js
@@ -1,7 +1,10 @@
 import { Redis } from '@upstash/redis'
 import bcrypt from 'bcryptjs'
 
-const redis = Redis.fromEnv()
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
